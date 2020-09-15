acd84b5f1fdae510b8ec48c06e937b1cc734c614
DInuwan97 <kalubowila.dc25132@live.com>
2020-09-15 11:43:04 +0530

    Initial Commit

---
+++ UserRegistrationApp/Controllers/ApplicationUserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserRegistrationApp.Models;
+
+namespace UserRegistrationApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST:api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationuser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber
+
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationuser, model.Password);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+           
+        }
+
+    }
+}
---
+++ UserRegistrationApp/Models/AuthenticationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistrationApp.Models
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
